6a11c1a96a177a440c506bdc544722f009acb661
Rustam <mirzamorf@yandex.ru>
2020-10-11 18:01:53 +0300

    Добавил новые методы для событий

--- src/core/Dom.js
+++ src/core/Dom.js
@@ -22,6 +22,14 @@ class Dom {
         this.$el.append(node)
         return this
     }
+
+    on(eventType, callback) {
+        this.$el.addEventListener(eventType, callback)
+    }
+
+    off(eventType, callback) {
+        this.$el.removeEventListener(eventType, callback)
+    }
 }
 
 export function $(selector) {
