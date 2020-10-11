a77e0e60265651567525344151ffa9b73aec2fd3
Rustam <mirzamorf@yandex.ru>
2020-10-11 18:02:33 +0300

    Добавил функционал событий

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,8 +1,21 @@
 import DomListener from '@core/DomListener'
 
 export default class ExcelComponent extends DomListener {
+    constructor($root, options = []) {
+        super($root, options.listeners)
+        this.name = options.name || ''
+    }
+
     // Возвращает шаблон компанента
     toHTML() {
         return ''
+    }
+
+    init() {
+        this.initDOMListeners()
+    }
+
+    destroy() {
+        this.removeDOMListeners()
     }
 }
